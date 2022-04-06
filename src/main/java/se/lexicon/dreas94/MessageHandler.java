18ff453dbff5e9d0e6a1f2802805bec13145e257
Andreas Eriksson <dreas94@gmail.com>
2022-04-06 15:37:10 +0200

    Added Message Handler

---
+++ src/main/java/se/lexicon/dreas94/MessageHandler.java
@@ -0,0 +1,19 @@
+package se.lexicon.dreas94;
+
+public class MessageHandler
+{
+
+    public static void msg(String text)
+    {
+        System.out.println(text);
+    }
+
+    public static void baseWarning()
+    {
+        baseWarning("");
+    }
+    public static void baseWarning(String text)
+    {
+        msg(text + "::You did something wrong with the input, default value used instead");
+    }
+}
